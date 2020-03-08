fa5cdd2d0733cda6cbf9ae97ae48f00551da6786
Samuel O'Donnell <samuel.odonnell@elementsuite.com>
2020-03-08 13:54:33 +0000

    adding async function

--- index.js
+++ index.js
@@ -1,41 +1,45 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
 
-var getBranches = function(client, pullRequest) {
-  return client.repos.listBranches({
+var getBranches = async function(client, pullRequest) {
+  return await client.repos.listBranches({
     owner: pullRequest.owner,
     repo: pullRequest.repo
   });
 }
 
-try {
-  const token = core.getInput('github-token');
-  const labelRegex = core.getInput('label-regex');
-  const client = new github.GitHub(token);
-  const payload = github.context.payload;
-  const pullRequest = github.context.issue;
-  const labels = payload.pull_request.labels;
+var main = async function() {
+  try {
+    const token = core.getInput('github-token');
+    const labelRegex = core.getInput('label-regex');
+    const client = new github.GitHub(token);
+    const payload = github.context.payload;
+    const pullRequest = github.context.issue;
+    const labels = payload.pull_request.labels;
 
 
-  if (!labels.length) {
-    return;
-  }
-
-  let labelBranches = [];
-  for (var i = 0; i < labels.length; i++) {
-    let label = labels[i];
-    if (new RegExp(labelRegex, 'i').test(label.name)) {
-      labelBranches.push(label.name);
+    if (!labels.length) {
+      return;
     }
+
+    let labelBranches = [];
+    for (var i = 0; i < labels.length; i++) {
+      let label = labels[i];
+      if (new RegExp(labelRegex, 'i').test(label.name)) {
+        labelBranches.push(label.name);
+      }
+    }
+
+    console.log("sam debug", labelBranches);
+
+    let repoBranches = getBranches(client, pullRequest);
+
+    console.log("sam debug", repoBranches);
+
+
+  } catch (error) {
+    console.error(error.message);
   }
-
-  console.log("sam debug", labelBranches);
-
-  let repoBranches = getBranches(client, pullRequest);
-
-  console.log("sam debug", repoBranches);
-
-
-} catch (error) {
-  console.error(error.message);
 }
+
+main();
