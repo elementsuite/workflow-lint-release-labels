76f5ff997abe56cd96ce8ddb921d03f505fc6764
Samuel O'Donnell <samuel.odonnell@elementsuite.com>
2020-03-08 13:39:50 +0000

    auto merge prs based on labels

---
+++ README.md
@@ -0,0 +1,1 @@
+# Lint Pull Request Title
---
+++ index.js
@@ -0,0 +1,39 @@
+const core = require('@actions/core');
+const github = require('@actions/github');
+
+var getBranches = function(client, pullRequest) {
+  return client.repos.listBranches({
+    owner: pullRequest.owner,
+    repo: pullRequest.repo
+  });
+}
+
+try {
+  const token = core.getInput('github-token');
+  const labelRegex = core.getInput('label-regex');
+  const client = new github.GitHub(token);
+  const payload = github.context.payload;
+  const pullRequest = github.context.issue;
+  const labels = payload.pull_request.labels;
+
+
+  if (!labels.length) {
+    return;
+  }
+
+  let labelBranches = [];
+  for (var i = 0; i < labels.length; i++) {
+    let label = labels[i];
+    if (new RegExp(labelRegex, 'i').test(label.name)) {
+      labelBranches.push(label.name);
+    }
+  }
+
+  console.log("sam debug", labelBranches);
+
+  //let repoBranches = getBranches(client, pullRequest);
+
+
+} catch (error) {
+  console.error(error.message);
+}
